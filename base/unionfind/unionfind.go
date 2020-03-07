cbd6a3a4a5fd0f4906673533f912854e91c77e48
Rangding Zhang <love-nankai@163.com>
2020-03-07 10:44:51 +0800

    Update unionfind.go

--- base/unionfind/unionfind.go
+++ base/unionfind/unionfind.go
@@ -3,6 +3,8 @@ package unionfind
 /*
 一个非常有趣的讲解： https://blog.csdn.net/niushuai666/article/details/6662911
 */
+
+// 用数组实现，也可以定义节点建立森林
 type UnionFind []int
 
 func NewUnionFind(n int) UnionFind {
@@ -16,21 +18,19 @@ func NewUnionFind(n int) UnionFind {
 /* 递归实现find：
 func (uf UnionFind) Find(x int) int {
 	if uf[x] != x {
-		uf[x] = uf.find(uf[x])
+		uf[x] = uf.find(uf[x]) // 路径压缩
 	}
 	return uf[x]
 }
 */
 func (uf UnionFind) Find(x int) int {
-	root := x
-	for root != uf[root] {
-		root = uf[root]
+	for uf[x] != x { // 路径压缩
+		uf[x] = uf[uf[x]]
+		x = uf[x]
 	}
-	for root != x {
-		uf[x], x = root, uf[x]
-	}
-	return root
+	return uf[x]
 }
 func (uf UnionFind) Join(x, y int) {
-	uf[x] = y
+	rootX, rootY := uf.Find(x), uf.Find(y)
+	uf[rootX] = rootY   // 可以按秩合并，即高度较小的树根插入高度较大的树根下面，进一步减少整个Union、Find操作的复杂度
 }
