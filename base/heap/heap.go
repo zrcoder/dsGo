b8d1ccd01c209da233d2a9780ff36b06a86f882a
zrcoder <love-nankai@163.com>
2020-05-25 10:05:00 +0800

    *

--- base/heap/heap.go
+++ base/heap/heap.go
@@ -1,54 +1,120 @@
 package heap
 
-import (
-	"container/heap"
-)
+type Cmp func(i, j int) bool
+type Any interface{}
+
+type Heap interface {
+	InitWithCmp(cmp Cmp)
+	Get(i int) Any
+	Len() int
+	Peek() Any
+	Push(x Any)
+	Pop() Any
+	Fix(i int)
+	Remove(i int) Any
+}
 
 func New() Heap {
 	return NewWithCap(0)
 }
 
 func NewWithCap(cap int) Heap {
-	return &heapImp{inner: &helper{slice: make([]Any, 0, cap)}}
+	return &heapImp{slice: make([]Any, 0, cap)}
 }
 
 func NewWithSlice(slice []Any) Heap {
-	return &heapImp{inner: &helper{slice: slice}}
+	return &heapImp{slice: slice}
 }
 
 type heapImp struct {
-	inner *helper
+	cmp   Cmp
+	slice []Any
 }
 
 func (h *heapImp) InitWithCmp(cmp Cmp) {
-	h.inner.cmp = cmp
-	heap.Init(h.inner)
+	h.cmp = cmp
+	n := h.Len()
+	for i := n/2 - 1; i >= 0; i-- {
+		h.down(i, n)
+	}
 }
 
 func (h *heapImp) Get(i int) Any {
-	return h.inner.slice[i]
+	return h.slice[i]
 }
 
 func (h *heapImp) Len() int {
-	return h.inner.Len()
+	return len(h.slice)
 }
 
 func (h *heapImp) Peek() Any {
-	return h.inner.slice[0]
+	return h.slice[0]
 }
 
 func (h *heapImp) Push(x Any) {
-	heap.Push(h.inner, x)
+	h.slice = append(h.slice, x)
+	h.up(h.Len() - 1)
 }
 
 func (h *heapImp) Pop() Any {
-	return heap.Pop(h.inner)
+	n := h.Len() - 1
+	h.swap(0, n)
+	h.down(0, n)
+	result := h.slice[n]
+	h.slice = h.slice[:n]
+	return result
 }
 
 func (h *heapImp) Fix(i int) {
-	heap.Fix(h.inner, i)
+	if !h.down(i, h.Len()) {
+		h.up(i)
+	}
 }
 
 func (h *heapImp) Remove(i int) Any {
-	return heap.Remove(h.inner, i)
+	n := h.Len() - 1
+	if n != i {
+		h.swap(i, n)
+		if !h.down(i, n) {
+			h.up(i)
+		}
+	}
+	result := h.slice[n]
+	h.slice = h.slice[:n]
+	return result
+}
+
+func (h *heapImp) swap(i, j int) {
+	h.slice[i], h.slice[j] = h.slice[j], h.slice[i]
+}
+
+func (h *heapImp) up(j int) {
+	for {
+		i := (j - 1) / 2 // parent
+		if i == j || !h.cmp(j, i) {
+			break
+		}
+		h.swap(i, j)
+		j = i
+	}
+}
+
+func (h *heapImp) down(i0, n int) bool {
+	i := i0
+	for {
+		j1 := 2*i + 1
+		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
+			break
+		}
+		j := j1 // left child
+		if j2 := j1 + 1; j2 < n && h.cmp(j2, j1) {
+			j = j2 // = 2*i + 2  // right child
+		}
+		if !h.cmp(j, i) {
+			break
+		}
+		h.swap(i, j)
+		i = j
+	}
+	return i > i0
 }
--- base/heap/readme.md
+++ base/heap/readme.md
@@ -168,10 +168,114 @@ func (h *helper) Pop() interface{} {
 	return x
 }
 ```
-#### 2. 参考标准库核心方法 up 和 down ，从头实现heapImp
-代码待补充~
+#### 2. 参考标准库核心方法 up 和 down ，从头写heapImp
+```go
+func New() Heap {
+	return NewWithCap(0)
+}
 
-### 扩展 Api 探讨
-标准库还提供了Fix 和 Remove 方法， 都是操作指定索引处的元素，我还没想到实际使用场景；
-实际上倒是有删除堆里某个元素的诉求，只不过传入的是元素值，而不是索引， 这个可以通过遍历内部切片先产找到元素索引，再调用标准库的 Remove 方法来实现。
-当前实现的 Fix、Remove，还有用于 InitWithCmp 的 Get 方法，都是针对指定索引。
+func NewWithCap(cap int) Heap {
+	return &heapImp{slice: make([]Any, 0, cap)}
+}
+
+func NewWithSlice(slice []Any) Heap {
+	return &heapImp{slice: slice}
+}
+
+type heapImp struct {
+	cmp   Cmp
+	slice []Any
+}
+
+func (h *heapImp) InitWithCmp(cmp Cmp) {
+	h.cmp = cmp
+	n := h.Len()
+	for i := n/2-1; i >= 0; i-- {
+		h.down(i, n)
+	}
+}
+
+func (h *heapImp) Get(i int) Any {
+	return h.slice[i]
+}
+
+func (h *heapImp) Len() int {
+	return len(h.slice)
+}
+
+func (h *heapImp) Peek() Any {
+	return h.slice[0]
+}
+
+func (h *heapImp) Push(x Any) {
+	h.slice = append(h.slice, x)
+	h.up(h.Len()-1)
+}
+
+func (h *heapImp) Pop() Any {
+	n := h.Len() - 1
+	h.swap(0, n)
+	h.down(0, n)
+	result := h.slice[n]
+	h.slice = h.slice[:n]
+	return result
+}
+
+func (h *heapImp) Fix(i int) {
+	if !h.down(i, h.Len()) {
+		h.up(i)
+	}
+}
+
+func (h *heapImp) Remove(i int) Any {
+	n := h.Len() - 1
+	if n != i {
+		h.swap(i, n)
+		if !h.down(i, n) {
+			h.up(i)
+		}
+	}
+	result := h.slice[n]
+	h.slice = h.slice[:n]
+	return result
+}
+
+func (h *heapImp) swap(i, j int) {
+	h.slice[i], h.slice[j] = h.slice[j], h.slice[i]
+}
+
+func (h *heapImp) up(j int) {
+	for {
+		i := (j - 1) / 2 // parent
+		if i == j || !h.cmp(j, i) {
+			break
+		}
+		h.swap(i, j)
+		j = i
+	}
+}
+
+func (h *heapImp) down(i0, n int) bool {
+	i := i0
+	for {
+		j1 := 2*i + 1
+		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
+			break
+		}
+		j := j1 // left child
+		if j2 := j1 + 1; j2 < n && h.cmp(j2, j1) {
+			j = j2 // = 2*i + 2  // right child
+		}
+		if !h.cmp(j, i) {
+			break
+		}
+		h.swap(i, j)
+		i = j
+	}
+	return i > i0
+}
+```
+### Get、Fix 和 Remove
+Get用来获取指定索引元素，主要用在InitWithCmp方法中；
+Fix 和 Remove 方法， 都是操作指定索引处的元素，我还没想到实际使用场景；
+实际上倒是有删除堆里某个元素的诉求，只不过传入的是元素值，而不是索引， 这个可以通过遍历内部切片先产找到元素索引，再调用当前 Remove 方法来实现。
